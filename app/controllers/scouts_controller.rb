27fd2e5252f9b9496f6cd043b23d27a17cd6e946
Adam Tse <mr.adam.tse@gmail.com>
2016-11-06 18:14:08 -0500

    refactoring code

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -1,6 +1,6 @@
 class ScoutsController < ApplicationController
   def index
-      client = F00px::Client.new
+      client = FivePx.new
       response = client.get('photos?rpp=100')
       @photos  = JSON.parse(response.body)['photos']
   end
--- app/models/five_px.rb
+++ app/models/five_px.rb
@@ -1,8 +1,11 @@
 class FivePx
-  def initialize(token, secret)
-      @client              = F00px::Client.new
-      @client.token        = session[:token]
-      @client.token_secret = session[:token_secret]
+  def initialize(token = nil, secret = nil)
+      F00px::Client.new
+      @client                = F00px::Client.new
+      if !token.nil?
+        @client.token        = session[:token]
+        @client.token_secret = session[:token_secret]
+      end
   end
 
  private
