70cbb05b2192dacae0a427910ee87faa473ddaf1
Adam Tse <mr.adam.tse@gmail.com>
2016-11-06 15:12:13 -0500

    checking dependencies

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -1,13 +1,11 @@
 class ScoutsController < ApplicationController
   def index
     # client = FivePx.new(session[:oauth_token], session[:oauth_secret])
-    client = client = F00px::Client.new
-      client.token        = session[:token]
-      client.token_secret = session[:token_secret]
+      client = F00px::Client.new
 
     response = client.get('photos')
     puts response.body
-    client.get('photos')
+    render text: response.body
   end
 
   def create
