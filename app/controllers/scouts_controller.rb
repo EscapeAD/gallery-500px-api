af65dbc2934830f80442a3f45324737449843921
Adam Tse <mr.adam.tse@gmail.com>
2016-11-06 17:55:15 -0500

    gallery for top 100 created on main page

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -2,10 +2,8 @@ class ScoutsController < ApplicationController
   def index
     # client = FivePx.new(session[:oauth_token], session[:oauth_secret])
       client = F00px::Client.new
-
-    response = client.get('photos')
-    puts response.body
-    render text: response.body
+      response = client.get('photos?rpp=100')
+      @photos  = JSON.parse(response.body)['photos']
   end
 
   def create
