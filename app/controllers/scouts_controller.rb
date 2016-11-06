0f0d3ff0fea638bf6af30da9f1e80aa49a19949f
Adam Tse <mr.adam.tse@gmail.com>
2016-11-06 18:54:15 -0500

    added text limit

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -1,13 +1,20 @@
 class ScoutsController < ApplicationController
+  before_action: set_client
   def index
-      client = FivePx.new
-      response = client.get('photos?rpp=100')
+      response = client.get('photos?rpp=100&feature=popular')
       @photos  = JSON.parse(response.body)['photos']
+  end
+
+  def show
+    response = client.get('photos?rpp=100&feature=popular')
   end
 
   def create
 
   end
 
-
+private
+  def set_client
+    client = F00px::Client.new
+  end
 end
