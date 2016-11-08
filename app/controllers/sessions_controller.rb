329fdc786a517cccf6af62f925acf842408f164d
Adam Tse <mr.adam.tse@gmail.com>
2016-11-07 20:01:39 -0500

    modify for test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,19 @@ class SessionsController < ApplicationController
   def create
     session[:token]        = auth_hash['credentials']['token']
     session[:token_secret] = auth_hash['credentials']['secret']
-    session[:username]     = request.env['omniauth.auth']['info']['username']
-    render json:  request.env['omniauth.auth']
-    # redirect_to root_path
+    session[:username]     = auth_hash['info']['email']
+    # render json:  request.env['omniauth.auth']
+    puts session[:token]
+    puts session[:token_secret]
+    puts session[:username]
+    redirect_to root_path
   end
 
   def destroy
-    session.each do |info|
-      info = nil
-    end
+    session[:token]        = nil
+    session[:token_secret] = nil
+    session[:username]     = nil
+    redirect_to root_path
   end
 
   protected
