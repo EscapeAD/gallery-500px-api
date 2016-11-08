ed356f5fa7eb679f32ebbc1dc9bec5d3a726fa1d
Adam Tse <mr.adam.tse@gmail.com>
2016-11-07 19:41:59 -0500

    added logout/added session delete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    session[:token]        = auth_hash.token
-    session[:token_secret] = auth_hash.secret
+    session[:token]        = auth_hash['credentials']['token']
+    session[:token_secret] = auth_hash['credentials']['secret']
+    session[:username]     = request.env['omniauth.auth']['info']['username']
     render json:  request.env['omniauth.auth']
     # redirect_to root_path
+  end
+
+  def destroy
+    session.each do |info|
+      info = nil
+    end
   end
 
   protected
 
   def auth_hash
-    request.env['omniauth.auth']['credentials']
+    request.env['omniauth.auth']
   end
 end
