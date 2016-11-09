38e1684edbee2f3f572087f7a80498c7b6abd74a
Adam Tse <mr.adam.tse@gmail.com>
2016-11-09 14:31:50 -0500

    basic test

--- test/controllers/scouts_controller_test.rb
+++ test/controllers/scouts_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ScoutsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index test" do
+    get '/'
+    assert_select 'h1', '500PX SCOUT'
+  end
+
+  test "scout search" do
+    get '/scouts/:user', params: { search: 'sofia'}
+    assert_select 'h2', 'sofia'
+  end
+
 end
