d258e89e9c701d54c43d18e6f719902a378ac567
Adam Tse <mr.adam.tse@gmail.com>
2016-11-11 14:07:33 -0500

    refactor destory method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:token]        = nil
-    session[:token_secret] = nil
-    session[:username]     = nil
-    session[:key]          = nil
-    session[:secret]       = nil
+    session.each_value do |value|
+      value = nil
+    end
+    # session[:token]        = nil
+    # session[:token_secret] = nil
+    # session[:username]     = nil
+    # session[:key]          = nil
+    # session[:secret]       = nil
     redirect_to root_path
   end
 
